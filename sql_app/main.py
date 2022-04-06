a9c35b3a68b0d2907fba69aeb3bdffbdb6b60f69
Leah Stern <lstern@appacademy.io>
2022-04-06 16:00:17 -0400

    set up CSRF config, CSRF exception handling, and incorporated CSRF protection into signup form POST route

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from fastapi_csrf_protect import CsrfProtect
+from fastapi_csrf_protect.exceptions import CsrfProtectError
+from pydantic import BaseModel
+
 from sql_app.routers import user_routes, auth_routes
+
+import os
 
 # create all DB tables
 # now commented out because this is done with Alembic
@@ -12,7 +20,34 @@ app = FastAPI()
 app.include_router(user_routes)
 app.include_router(auth_routes)
 
+# CSRF settings class, loads secret key from .env
+class CsrfSettings(BaseModel):
+    secret_key : str = os.environ.get("SECRET_KEY")
+
+# loads CsrfSettings class into CsrfProtect configuration
+@CsrfProtect.load_config
+def get_csrf_config():
+    return CsrfSettings()
+
 # Root route
 @app.get("/")
 def index():
     return {"Hello": "World"}
+
+"""
+Route for custom CSRF protection exception handling.
+
+If a CSRF exception is encountered in another route, that route simply needs
+to raise a CsrfProtectError with a status_code and message, for example:
+
+if condition:
+    raise CsrfProtectError(status_code=404, message="Something went wrong!")
+
+and it will be rerouted to and handled by this exception handler route.
+"""
+@app.exception_handler(CsrfProtectError)
+def csrf_exception_handler(request: Request, exception: CsrfProtectError):
+    return JSONResponse(
+        status_code=exception.status_code,
+        content={"error": exception.message}
+    )
--- sql_app/routers/auth_routes.py
+++ sql_app/routers/auth_routes.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Form, Request
-from sql_app import seeder, models
+from fastapi import APIRouter, Form, Request, Depends
+
+from fastapi_csrf_protect import CsrfProtect
+from fastapi_csrf_protect.exceptions import CsrfProtectError
+
+from sql_app import models
 from sql_app.database import get_db_sesh
-from fastapi.responses import HTMLResponse
 
 # class that allows for separate routers (like Blueprints in Flask)
 auth_routes = APIRouter(
@@ -20,9 +23,19 @@ NOTE: currently, this app does not include a frontend nor SSR, so you'll need
 to use Postman or a similar platform to send this POST request
 """
 @auth_routes.post("/signup/")
-def signup(first: str = Form(...), last: str = Form(...)):
+def signup(request: Request, first: str = Form(...), last: str = Form(...), csrf_protect: CsrfProtect = Depends()):
+    # try to get CSRF token from request header and validate it. Otherwise raise CsrfProtectError to be handled by
+    # the csrf_protection_handler route
+    try:
+        csrf_token = csrf_protect.get_csrf_from_headers(request)
+        csrf_protect.validate_csrf(csrf_token)
+    except:
+        raise CsrfProtectError(status_code=500, message="Could not validate CSRF token!")
+
+    # get database session
     db = next(get_db_sesh())
 
+    # add new user to databse
     if db:
         new_user = models.User(
             first_name=first,
